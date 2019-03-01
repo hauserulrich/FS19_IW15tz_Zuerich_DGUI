0515431f7090b481ed591fcace82484b5dea0ee4
Tomeig <thomas.eigenmann@protonmail.ch>
2019-03-01 12:21:19 +0100

    Austesten potentieller API's (OSM)(OPW)

--- www/js/main.js
+++ www/js/main.js
@@ -4,6 +4,18 @@
  */
 function init()
 {
+
+/** OSM Integration of the MAP */
+var Longitude = 46.852704
+
+var Latitude = 9.5255747
+
+var mymap = L.map('mapid').setView([Longitude, Latitude], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+
 }
 
 /** initialise */
