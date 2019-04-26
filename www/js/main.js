acc1628fb9281115486553da5473417240dd3d88
hirsbrrebekk <rebekka.hirsbrunner@iw.htwchur.ch>
2019-04-26 21:19:52 +0200

    json weather here eingebunden

--- www/js/main.js
+++ www/js/main.js
@@ -6,4 +6,18 @@ $("[data-toggle=popover]").popover({
         }
 });
 
-
+$.ajax({
+	url: 'https://weather.api.here.com/weather/1.0/report.json',
+	type: 'GET',
+	dataType: 'jsonp',
+	jsonp: 'jsonpcallback',
+	data: {
+		product: 'alerts',
+		name: 'Lucerne',
+		app_id: 'zvWSTyjcoSOSaHgDd47m',
+		app_code: 'JW0IQrm5Unp63DYjintgxg'
+	},
+	success: function (data) {
+		console.log(JSON.stringify(data));
+	}
+});
