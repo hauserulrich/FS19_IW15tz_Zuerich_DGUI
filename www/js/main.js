204dd3aae09349502b6ca4e5b6a61d3bba70435c
hirsbrrebekk <rebekka.hirsbrunner@iw.htwchur.ch>
2019-05-10 08:58:43 +0200

    weather Bern eingebunden

--- www/js/main.js
+++ www/js/main.js
@@ -18,14 +18,18 @@ function getWeatherJSON(){
 		jsonp: 'jsonpcallback',
 		data: {
 			product: 'observation',
-			name: 'Lucerne',
+			name: 'Bern',
 			app_id: 'zvWSTyjcoSOSaHgDd47m',
 			app_code: 'JW0IQrm5Unp63DYjintgxg'
 		},
 		success: function (data) {
 			console.log(data);
-			let location = data.observations;
+			let location = data.observations.location[0].state;
 			$('.card-title:first').text(location);
+			let temp = data.observations.location[0].observation[0].temperature;
+			$('.card-subtitle:first').text(temp);
+			let description = data.observations.location[0].observation[0].description;
+			$('.description:first').text(description);
 		}
 	});
 }
