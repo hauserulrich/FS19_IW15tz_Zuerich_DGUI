179c8089b3586ca2bd7ecde654c1472259aa5149
zimmernicole <nicole.zimmermann@iw.htwchur.ch>
2019-05-23 18:21:05 +0200

    Card Loop

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,5 @@
+var cities=["Bern", "Zurich", "Luzern", "Genf", "Chur"];
+var chosenCity="";
 
 $("[data-toggle=popover]").popover({
     html: true, 
@@ -7,10 +9,19 @@ $("[data-toggle=popover]").popover({
 });
 
 $(document).ready(function(){
-	getWeatherJSON();
+	cityLoop();
+	//createCard();
+	//getWeatherJSON();
 });
 
-function getWeatherJSON(){
+function cityLoop(){
+	for (i in cities){
+		chosenCity=cities[i];
+		getWeatherJSON();
+	}
+};
+
+function getWeatherJSON(){	
 	$.ajax({
 		url: 'https://weather.api.here.com/weather/1.0/report.json',
 		type: 'GET',
@@ -18,19 +29,23 @@ function getWeatherJSON(){
 		jsonp: 'jsonpcallback',
 		data: {
 			product: 'observation',
-			name: 'Bern',
+			name: chosenCity,
+			//name: 'Luzern',
 			app_id: 'zvWSTyjcoSOSaHgDd47m',
 			app_code: 'JW0IQrm5Unp63DYjintgxg'
 		},
 		success: function (data) {
 			console.log(data);
-			var loc		
+			var loc
+			//createCard();		
 			for (i in data.observations.location) {
 				  if (data.observations.location[i].distance==0){
 				  loc=data.observations.location[i];
 				  }
 				}
-			//let location = data.observations.location[0];
+			createCard();
+
+			//let location = data.observations.location[0]
 			let city = loc.state;
 			$('.card-title:first').text(city);
 			let temp = loc.observation[0].temperature;
@@ -41,4 +56,38 @@ function getWeatherJSON(){
 			$('.weathericon:first').attr('src', icon);
 		}
 	});
-}
+};
+
+function createCard(){
+	var x=document.getElementById("localWeather");
+	var divcolsm=document.createElement("div");
+	x.appendChild(divcolsm);
+	divcolsm.className="col-sm";
+
+    var divcard=document.createElement("div");
+	divcolsm.appendChild(divcard);
+	divcard.className="card";
+
+	var divcardbody=document.createElement("div");
+	divcard.appendChild(divcardbody);
+	divcardbody.className="card-body";
+
+	var cardtitle=document.createElement("h4");
+	divcardbody.appendChild(cardtitle);
+	cardtitle.className="card-title"
+	cardtitle.innerHTML="title";
+
+	var cardsubtitle=document.createElement("h4");
+	divcardbody.appendChild(cardsubtitle);
+	cardsubtitle.className="card-subtitle mb-2 text-muted";
+	cardsubtitle.innerHTML="subtitle";
+
+	var cardicon=document.createElement("img");
+	divcardbody.appendChild(cardicon);
+	cardicon.className="weathericon";
+	cardicon.src="...";
+
+	var carddescription=document.createElement("a");
+	divcardbody.appendChild(carddescription);
+	carddescription.className="description";
+};
